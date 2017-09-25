d3e76749e7732aebbfc6500c662b36ea97a425b0
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-09-25 12:22:08 -0600

    added a string change and a string interpolation.

--- Testing.playground/Contents.swift
+++ Testing.playground/Contents.swift
@@ -2,4 +2,13 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var myName :String
+var otherName = "words"
+var thisName :String = "Perth is OP"
+
+
+var thisString :String = "String change"
+
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+// message is "3 times 2.5 is 7.5"
