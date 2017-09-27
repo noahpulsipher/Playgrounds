f42a2dad42dcea0c2c8d816cf8cd1c9aba28de3f
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-09-27 11:55:10 -0600

    added a playgrounds for method testing

---
+++ MethodIfs.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple() -> Void
+{
+    print("Jian is a bully")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String) -> Void
+{
+    let answer = "My name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "slim shady")
+let words = "MarshalL Mathers"
+aBitLessSimple(name: words)
