04f12e4656d00fe599d99e746cc7a17ad2731131
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-02 12:24:52 -0600

    created a playground to practice classes and inits

---
+++ Classes.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var notesToday = "Using Swift to make classes and methods"
+
+public class SimpleClass
+{
+    private var name :String
+    
+    public init()
+    {
+        name = "My name is ...."
+    }
+        
+}
