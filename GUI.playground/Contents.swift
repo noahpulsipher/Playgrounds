a28f95fc0c04c40826f7562a5aa3b85d9e53fb0d
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-04 13:46:33 -0600

    Added comments to help me know what I'm looking at and how they work

--- GUI.playground/Contents.swift
+++ GUI.playground/Contents.swift
@@ -4,6 +4,7 @@ import UIKit
 
 var topic = "Working with GUI XD"
 
+//How to make views
 var firstView = UIView(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
 firstView.backgroundColor = .purple
 
@@ -13,7 +14,7 @@ secondView.backgroundColor = .yellow
 var thirdView = UIView(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
 thirdView.backgroundColor = .red
 
-
+//How to make buttons
 var myFirstButton = UIButton(frame: CGRect(x:50, y: 70, width: 200, height:150))
 myFirstButton.setTitle("Words", for: .normal)
 myFirstButton.backgroundColor = .blue
@@ -22,6 +23,7 @@ var secondButton = UIButton(frame: CGRect(x: 50, y: 70, width: 200, height: 150)
 secondButton.setTitle("Hi", for: .normal)
 secondButton.backgroundColor = .green
 
+//How to make labels
 var firstLabel = UILabel(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
 firstLabel.text = "Hello"
 firstLabel.textColor = .white
@@ -30,6 +32,7 @@ var secondLabel = UILabel(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
 secondLabel.text = "Yes"
 secondLabel.backgroundColor = .white
 
+//How to make a date picker
 var firstDatePicker = UIDatePicker(frame: CGRect(x: 100, y: 70, width: 500, height: 500))
 firstDatePicker.backgroundColor = .white
 
