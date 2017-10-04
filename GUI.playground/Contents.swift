cb48972a41b3d2f7b9346a7dd4d82180035b691c
noahpulsipher <31518741+noahpulsipher@users.noreply.github.com>
2017-10-04 13:41:41 -0600

    Created buttons, view, labels, and a date picker and snapped a photo of each

--- GUI.playground/Contents.swift
+++ GUI.playground/Contents.swift
@@ -4,6 +4,34 @@ import UIKit
 
 var topic = "Working with GUI XD"
 
-var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height:120))
+var firstView = UIView(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+firstView.backgroundColor = .purple
+
+var secondView = UIView(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+secondView.backgroundColor = .yellow
+
+var thirdView = UIView(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+thirdView.backgroundColor = .red
+
+
+var myFirstButton = UIButton(frame: CGRect(x:50, y: 70, width: 200, height:150))
 myFirstButton.setTitle("Words", for: .normal)
-myFirstButton.backgroundColor = .green
+myFirstButton.backgroundColor = .blue
+
+var secondButton = UIButton(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+secondButton.setTitle("Hi", for: .normal)
+secondButton.backgroundColor = .green
+
+var firstLabel = UILabel(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+firstLabel.text = "Hello"
+firstLabel.textColor = .white
+
+var secondLabel = UILabel(frame: CGRect(x: 50, y: 70, width: 200, height: 150))
+secondLabel.text = "Yes"
+secondLabel.backgroundColor = .white
+
+var firstDatePicker = UIDatePicker(frame: CGRect(x: 100, y: 70, width: 500, height: 500))
+firstDatePicker.backgroundColor = .white
+
+
+
